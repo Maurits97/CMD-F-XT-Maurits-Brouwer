f7e82b7ebc64758116b3111a83835d5a7f0d252c
Maurits Brouwer <>
2018-03-14 21:21:18 +0100

    major changes

--- C2/scripts/script.js
+++ C2/scripts/script.js
@@ -6,6 +6,15 @@ function timer() {
 	var year = date.getFullYear();
 	var day = date.getUTCDate();
 	
+	if (hours < 10) {
+		hours = "0" + hours;
+	} 
+	if (minutes < 10) {
+		minutes = "0" + minutes;
+	} 
+	if (seconds < 10) {
+		seconds = "0" + seconds;
+	}
 	var month = new Array();
 		month[0] = "January";
 		month[1] = "February";
@@ -20,37 +29,70 @@ function timer() {
 		month[10] = "November";
 		month[11] = "December";
 	var newMonth = month[date.getMonth()];
-	
-	if (minutes < 10) {
-		minutes = "0" + minutes;
-	} else if (hours < 10) {
-		hours = "0" + hours;
-	} else if (seconds < 10) {
-		seconds = "0" + seconds;
-	}
-	
+
 	var time = hours + ":" + minutes + ":" + seconds;
+
 	document.getElementById("time").innerHTML = time;
 	document.getElementById("date").innerHTML = day + " " + newMonth +" " + year;
 
-	if (hours > 6 && hours < 12) {
-		document.getElementById("title").innerHTML = "Good morning,";
+	var title = document.getElementById("title");
+	var part = document.getElementById("part");
+	var its = document.getElementById("its");
+
+
+	if (hours > 5 && hours < 12) {
+		title.innerHTML = "Good morning,";
 		document.getElementById("img1").style.display = "block";
-	} else if (hours > 12 && hours < 14) {
-		document.getElementById("title").innerHTML = "Good afternoon,";
+		document.getElementById("img0").style.display = "none";
+		part.innerHTML = "time for breakfast";
+		part.style.display = "block";
+		its.style.display = "inline";
+
+	} else if (hours >= 12 && hours <= 14) {
+		title.innerHTML = "Good afternoon,";
 		document.getElementById("img2").style.display = "block";
-	} else if (hours > 18 && hours < 20) {
-		document.getElementById("title").innerHTML = "Good evening,";
-		document.getElementById("img3").style.display = "block";
-	} else if (hours > 23 && hours < 6) {
-		document.getElementById("title").innerHTML = "Good night,";
-		document.getElementById("img4").style.display = "block";
-	} else{
-		document.getElementById("title").innerHTML = "Good day,";
+		document.getElementById("img1").style.display = "none";
+		part.innerHTML = "time to lunch";
+		part.style.display = "block";
+		its.style.display = "inline";
+
+	} else if(hours > 14 && hours < 18){
 		document.getElementById("img0").style.display = "block";
+		document.getElementById("img2").style.display = "none";
+		title.innerHTML = "Good afternoon,";
+		part.style.display = "none";
+		its.style.display = "none";
+
+	} else if (hours >= 18 && hours < 20) {
+		document.getElementById("img3").style.display = "block";
+		document.getElementById("img0").style.display = "none";
+		part.innerHTML = "time for dinner";
+		part.style.display = "block";
+		its.style.display = "inline";
+
+	} else if (hours >= 20 && hours < 23){
+		document.getElementById("img0").style.display = "block";
+		document.getElementById("img3").style.display = "none";
+		title.innerHTML = "Good evening,";
+		part.style.display = "none";
+		its.style.display = "none";
+
+	}else if (hours >= 23 || hours <= 1) {
+		title.innerHTML = "Good night,";
+		document.getElementById("img4").style.display = "block";
+		document.getElementById("img0").style.display = "none";
+		part.innerHTML = "time to sleep";
+		part.style.display = "block";
+		its.style.display = "inline";
+
+	} else if (hours > 1 && hours <= 5){
+		title.innerHTML = "Good night,";
+		document.getElementById("img0").style.display = "block";
+		document.getElementById("img4").style.display = "none";
+		part.innerHTML = "time to sleep";
+		part.style.display = "block";
+		its.style.display = "inline";
 	}
-
-
 }
 
 setInterval(timer, 100);
@@ -62,5 +104,7 @@ var timeline = new TimelineMax({ repeat: -1});
 	timeline.to(dayCycle, 100, {
 		rotation: 360});
 
+// idee om zon daadwerkelijk te laten draaien. daylight cycle. 
+// Zon 6:00 - 18:00, maan 18:00 - 6:00
 
 
