46894db70537f58277c04a03e4cf12f861a41b8a
Maurits Brouwer <>
2018-03-19 19:52:31 +0100

    prevending double image

--- C2/scripts/script.js
+++ C2/scripts/script.js
@@ -38,57 +38,83 @@ function timer() {
 	var title = document.getElementById("title");
 	var part = document.getElementById("part");
 	var its = document.getElementById("its");
+	var img0 = document.getElementById("img0");
+	var img1 = document.getElementById("img1");
+	var img2 = document.getElementById("img2");
+	var img3 = document.getElementById("img3");
+	var img4 = document.getElementById("img4");
 
 
 	if (hours > 5 && hours < 12) { //Different time variables
 		title.innerHTML = "Good morning,";
-		document.getElementById("img1").style.display = "block";
-		document.getElementById("img0").style.display = "none";
+		img0.style.display = "none";
+		img1.style.display = "block";
+		img2.style.display = "none";
+		img3.style.display = "none";
+		img4.style.display = "none";
 		part.innerHTML = "time for breakfast";
 		part.style.display = "block";
 		its.style.display = "inline";
 
 	} else if (hours >= 12 && hours <= 14) {
 		title.innerHTML = "Good afternoon,";
-		document.getElementById("img2").style.display = "block";
-		document.getElementById("img1").style.display = "none";
+		img0.style.display = "none";
+		img1.style.display = "none";
+		img2.style.display = "block";
+		img3.style.display = "none";
+		img4.style.display = "none";
 		part.innerHTML = "time to lunch";
 		part.style.display = "block";
 		its.style.display = "inline";
 
 	} else if(hours > 14 && hours < 18){
-		document.getElementById("img0").style.display = "block";
-		document.getElementById("img2").style.display = "none";
+		img0.style.display = "block";
+		img1.style.display = "none";
+		img2.style.display = "none";
+		img3.style.display = "none";
+		img4.style.display = "none";
 		title.innerHTML = "Good afternoon,";
 		part.style.display = "none";
 		its.style.display = "none";
 
 	} else if (hours >= 18 && hours < 20) {
-		document.getElementById("img3").style.display = "block";
-		document.getElementById("img0").style.display = "none";
+		img0.style.display = "none";
+		img1.style.display = "none";
+		img2.style.display = "none";
+		img3.style.display = "block";
+		img4.style.display = "none";
 		part.innerHTML = "time for dinner";
 		part.style.display = "block";
 		its.style.display = "inline";
 
 	} else if (hours >= 20 && hours < 23){
-		document.getElementById("img0").style.display = "block";
-		document.getElementById("img3").style.display = "none";
+		img0.style.display = "block";
+		img1.style.display = "none";
+		img2.style.display = "none";
+		img3.style.display = "none";
+		img4.style.display = "none";
 		title.innerHTML = "Good evening,";
 		part.style.display = "none";
 		its.style.display = "none";
 
 	} else if (hours >= 23 || hours <= 1) {
+		img0.style.display = "none";
+		img1.style.display = "none";
+		img2.style.display = "none";
+		img3.style.display = "none";
+		img4.style.display = "block";
 		title.innerHTML = "Good night,";
-		document.getElementById("img4").style.display = "block";
-		document.getElementById("img0").style.display = "none";
 		part.innerHTML = "time to sleep";
 		part.style.display = "block";
 		its.style.display = "inline";
 
 	} else if (hours > 1 && hours <= 5){
 		title.innerHTML = "Good night,";
-		document.getElementById("img0").style.display = "block";
-		document.getElementById("img4").style.display = "none";
+		img0.style.display = "block";
+		img1.style.display = "none";
+		img2.style.display = "none";
+		img3.style.display = "none";
+		img4.style.display = "none";
 		part.innerHTML = "time to sleep";
 		part.style.display = "block";
 		its.style.display = "inline";
