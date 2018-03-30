c90f978caa153e650448fc42a4e1230c655e01bb
Maurits Brouwer <>
2018-03-30 16:54:35 +0200

    changes

--- C3/scripts/script.js
+++ C3/scripts/script.js
@@ -1,4 +1,6 @@
 var map;
+var temp;
+var windSpeed;
 function initMap() {
 
 	var airportIcon = {
@@ -30,14 +32,47 @@ function initMap() {
   	map: map,
   	icon: airportIcon
   });
+
+  getAPIdataWeather();
+
+  var contentString = '<div class="markerContent">Landingplatform: Schiphol Airport' + '<br />' +
+                      'Height: 10000m' + '<br />' +
+                      'Temperature: ' + temp + ' degrees' +'<br />' +
+                      'Windspeed: '+ windSpeed + ' km/h'+ '<br />'
+                      '</div>';
+
+  var infowindow = new google.maps.InfoWindow({
+      content: contentString
+      });
+
+  schiphol.addListener('click', function() {
+          infowindow.open(map, schiphol);
+        });
 }
 
 function getAPIdata() {
-
   // get latest weather
-  // fetch('https://rickandmortyapi.com/api/character/265')
   fetch('https://rickandmortyapi.com/api/character/')
-  
+  // parse to JSON format
+  .then(function(response) {
+    return response.json();
+  })
+  // render weather per day
+  .then(function(response) {
+    // show full JSON object
+    console.log(response.results);
+    var characterArray = response.results;
+    var random = characterArray[Math.floor(Math.random() * characterArray.length)];
+    document.getElementById("RM").src = random.image;
+  })
+  // catch error
+  .catch(function (error) {
+    console.error('Request failed', error);
+  });
+}
+function getAPIdataWeather() {
+  // get latest weather
+  fetch('http://api.openweathermap.org/data/2.5/weather?lat=52.311481&lon=4.766752&units=metric&APPID=612d97da4d3eabf8ec876d11c5cd608c')
   // parse to JSON format
   .then(function(response) {
     return response.json();
@@ -46,23 +81,18 @@ function getAPIdata() {
   // render weather per day
   .then(function(response) {
     // show full JSON object
-    console.log(response.results);
-
-    var characterArray = response.results;
-
-    var random = characterArray[Math.floor(Math.random() * characterArray.length)];
-
-    console.log(random.image);
-
-
-    document.getElementById("RM").src = random.image;
+    console.log(response);
+    windSpeed = response.wind.speed;
+    temp = response.main.temp;
   })
   // catch error
   .catch(function (error) {
     console.error('Request failed', error);
   });
 }
-getAPIdata();
 
+
+getAPIdata();
+getAPIdataWeather();
 
 
