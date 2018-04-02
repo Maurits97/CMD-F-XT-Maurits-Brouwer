8853547039edfd57b4baee0f2dfd0efa9ebda325
Maurits Brouwer <>
2018-04-02 11:13:46 +0200

    added marker info

--- C3/scripts/script.js
+++ C3/scripts/script.js
@@ -1,6 +1,12 @@
 var map;
 var temp;
 var windSpeed;
+var temp2;
+var windSpeed2;
+var temp3;
+var windSpeed3;
+var temp4;
+var windSpeed4;
 function initMap() {
 
 	var airportIcon = {
@@ -28,25 +34,61 @@ function initMap() {
   	icon: airportIcon
   });
   var groningen = new google.maps.Marker({
-  	position: {lat: 53.125665, lng:  6.577628},
+  	position: {lat: 53.125665, lng: 6.577628},
   	map: map,
   	icon: airportIcon
   });
 
   getAPIdataWeather();
+  getAPIdataWeather2();
+  getAPIdataWeather3();
+  getAPIdataWeather4();
 
-  var contentString = '<div class="markerContent">Landingplatform: Schiphol Airport' + '<br />' +
-                      'Height: 10000m' + '<br />' +
+  var contentSchiphol = '<div class="markerContent">Landingplatform: Schiphol Airport' + '<br />' +
+                      'Sea level: -3 m' + '<br />' +
                       'Temperature: ' + temp + ' degrees' +'<br />' +
                       'Windspeed: '+ windSpeed + ' km/h'+ '<br />'
                       '</div>';
+  var contentRotterdam = '<div class="markerContent">Landingplatform: Rotterdam The Hague Airport' + '<br />' +
+                      'Sea level: -4 m' + '<br />' +
+                      'Temperature: ' + temp2 + ' degrees' +'<br />' +
+                      'Windspeed: '+ windSpeed2 + ' km/h'+ '<br />'
+                      '</div>';
+  var contentEindhoven = '<div class="markerContent">Landingplatform: Eindhoven Airport' + '<br />' +
+                      'Sea level: 20 m' + '<br />' +
+                      'Temperature: ' + temp3 + ' degrees' +'<br />' +
+                      'Windspeed: '+ windSpeed3 + ' km/h'+ '<br />'
+                      '</div>';
+  var contentGroningen = '<div class="markerContent">Landingplatform: Groningen Airport Eelde' + '<br />' +
+                      'Sea level: 3 m' + '<br />' +
+                      'Temperature: ' + temp4 + ' degrees' +'<br />' +
+                      'Windspeed: '+ windSpeed4 + ' km/h'+ '<br />'
+                      '</div>';                    
 
-  var infowindow = new google.maps.InfoWindow({
-      content: contentString
+  var infoSchiphol = new google.maps.InfoWindow({
+      content: contentSchiphol
+      });
+  var infoRotterdam = new google.maps.InfoWindow({
+      content: contentRotterdam
+      });
+  var infoEindhoven = new google.maps.InfoWindow({
+      content: contentEindhoven
+      });
+  var infoGroningen = new google.maps.InfoWindow({
+      content: contentGroningen
       });
 
   schiphol.addListener('click', function() {
-          infowindow.open(map, schiphol);
+          infoSchiphol.open(map, schiphol);
+        });
+  rotterdam.addListener('click', function() {
+          infoRotterdam.open(map, rotterdam);
+        });
+  eindhoven.addListener('click', function() {
+          infoEindhoven.open(map, eindhoven);
+        });
+  groningen.addListener('click', function() {
+          infoGroningen.open(map, groningen);
         });
 }
 
@@ -64,6 +106,7 @@ function getAPIdata() {
     var characterArray = response.results;
     var random = characterArray[Math.floor(Math.random() * characterArray.length)];
     document.getElementById("RM").src = random.image;
+    document.querySelector("link[rel*='icon']").href = random.image;
   })
   // catch error
   .catch(function (error) {
@@ -90,9 +133,70 @@ function getAPIdataWeather() {
     console.error('Request failed', error);
   });
 }
-
+function getAPIdataWeather2() {
+  // get latest weather
+  fetch('http://api.openweathermap.org/data/2.5/weather?lat=51.955271&lon=4.439765&units=metric&APPID=612d97da4d3eabf8ec876d11c5cd608c')
+  // parse to JSON format
+  .then(function(response) {
+    return response.json();
+  })
+  
+  // render weather per day
+  .then(function(response) {
+    // show full JSON object
+    console.log(response);
+    windSpeed2 = response.wind.speed;
+    temp2 = response.main.temp;
+  })
+  // catch error
+  .catch(function (error) {
+    console.error('Request failed', error);
+  });
+}
+function getAPIdataWeather3() {
+  // get latest weather
+  fetch('http://api.openweathermap.org/data/2.5/weather?lat=51.458264&lon=5.389123&units=metric&APPID=612d97da4d3eabf8ec876d11c5cd608c')
+  // parse to JSON format
+  .then(function(response) {
+    return response.json();
+  })
+  
+  // render weather per day
+  .then(function(response) {
+    // show full JSON object
+    console.log(response);
+    windSpeed3 = response.wind.speed;
+    temp3 = response.main.temp;
+  })
+  // catch error
+  .catch(function (error) {
+    console.error('Request failed', error);
+  });
+}
+function getAPIdataWeather4() {
+  // get latest weather
+  fetch('http://api.openweathermap.org/data/2.5/weather?lat=53.125665&lon=6.577628&units=metric&APPID=612d97da4d3eabf8ec876d11c5cd608c')
+  // parse to JSON format
+  .then(function(response) {
+    return response.json();
+  })
+  
+  // render weather per day
+  .then(function(response) {
+    // show full JSON object
+    console.log(response);
+    windSpeed4 = response.wind.speed;
+    temp4 = response.main.temp;
+  })
+  // catch error
+  .catch(function (error) {
+    console.error('Request failed', error);
+  });
+}
 
 getAPIdata();
 getAPIdataWeather();
-
+getAPIdataWeather2();
+getAPIdataWeather3();
+getAPIdataWeather4();
 
